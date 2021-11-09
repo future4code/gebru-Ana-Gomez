64c4833dc4de576a0503fd992bf6e1cabe41549c
pepinodev <92928197+pepinodev@users.noreply.github.com>
2021-11-09 19:55:55 -0300

    Exercício de variável realizado

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,88 @@
+// Exercícios de Variáveis 
+/*
+Exercício 1 -
+Primeiro será impresso no console o valor de b (no caso 10)
+Depois será impresso o valor de a (10) e b (5)
+*/
+
+/*
+Exercício 2 -
+Será impresso no console o valor de a (10), valor de b (10), e o valor de c (a = 10), que no caso é o mesmo valor de a
+*/
+
+/*
+Exercício 3 - 
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+No caso, a variável p pode ser mudada para horasDia
+Já a variável t pode ser mudada para salarioDia
+*/
+
+
+
+/*Exercício de Escrita de Código
+
+let nome
+
+let idade
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+Imprimi o tipo das variáveis e deu "undefined", acredito que seja porque não atribui nenhum valor a elas.
+Portanto, não teria como o typeof descobrir qual o tipo de variável, se não tiver nenhum valor atribuido ali.
+*/
+
+/*Exercício 1 - e)
+Usei const e let para atribuir os valores, logo em seguida imprimi no console.
+const nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual sua idade?")
+console.log(nome)
+console.log(idade)*/
+
+
+/* Ao imprimir na tela o tipo das variáveis notei que mudou de "undefined" para "string" e "number".
+Acredito que tenha mudado por justamente atribuir valores referentes aos tipos das variáveis.*/
+
+/*Imprimi na tela usando o código:
+console.log("Olá", nome, "você tem", idade, "anos")
+*/
+
+// Exercício 2
+/*let temGato = prompt ("Você tem gato?")
+let temIrmao = prompt("Você tem irmãos?")
+let temTrabalho = prompt("Você está empregado atualmente?")
+
+console.log(temGato)
+console.log(temIrmao)
+console.log(temTrabalho)
+
+let temG = temGato
+let temI = temIrmao
+let temT = temTrabalho
+
+console.log("Você tem gato?", temG,)
+console.log("Você tem irmãos?", temI,)
+console.log("Você está empregado atualmente?", temT)*/
+
+//Exercício 3
+let a = 10
+let b = 25
+let c
+
+c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b)
+
+
+
+
+
+
+
+
+
