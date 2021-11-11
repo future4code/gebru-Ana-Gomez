6f9747d49bbbefb112d8ea69710452201aea2fda
pepinodev <92928197+pepinodev@users.noreply.github.com>
2021-11-11 15:01:33 -0300

    Exercícios operadores feito

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,78 @@
+// Exercícios de Interpretação de Código
+/* 1 -
+console.log("a. ", false)
+
+console.log("b. ", false) 
+
+console.log("c. ", true)
+
+console.log("d. ", true)
+*/
+
+/* 2 - 
+Será impresso "12" no console, pelo fato de ele não ter atribuido valor do tipo number à variável.*/
+
+/* 3 -
+No caso ele teria que escrever da seguinte maneira:
+
+let primeiroNumero = Number (prompt("Digite um numero!"))
+let segundoNumero = Number (prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)*/
+
+// Exercícios de Escrita de Código
+//1 -
+ //a)
+  /*const idadeUsuario = Number (prompt("Qual a sua idade?"))
+//b)
+const idadeMelhorAmigo = Number (prompt("Qual a idade do seu(sua) melhor amigo(a)?"))
+//c) 
+const idadeMaiorMenor = idadeUsuario > idadeMelhorAmigo
+console.log("Sua idade é maior do que a do seu melhor amigo?" + idadeMaiorMenor)
+//d)
+let idadeDiferenca = idadeUsuario - idadeMelhorAmigo
+console.log("A diferença de idade entre vocês é de",
+idadeDiferenca, "anos")*/
+
+// No exercício d) eu usei , ao invés de + porque aparentemente com + a frase fica sem espaçamento, imprimindo "A diferença de idade entre vocês é dexanos"
+
+//2 -
+ //a)
+/*const numeroPar = prompt("Insira aqui um número PAR")
+ //b)
+console.log (numeroPar % 2)*/
+ //c) O resultado do resto da divisão entre "numeroPar" % 2, sempre dará 0, independente do valor, desde que esse seja par.
+ //d) O resultado do resto da divisão entre "numeroPar" % 2, sempre dará 1, independente do valor, desde que esse seja ímpar.
+
+// 3 -
+//a)
+/*let idadeUsuario = prompt("Qual é a sua idade?")
+console.log (idadeUsuario * 12)
+//b)
+const idadeDias = 240
+console.log (idadeDias * 30)
+//c)
+const idadeHoras = 7200
+console.log (idadeHoras * 24)*/
+
+//4 - 
+let n1 = prompt("Digite um número aleatório")
+let n2 = prompt("Digite outro número aleatório")
+
+/*const nMaior = n1 > n2
+console.log("O primeiro número é maior que o segundo?" + nMaior)*/
+/*const nIgual = n1 === n2
+console.log("O primeiro número é igual ao segundo?" + nIgual)*/
+
+/*const n1Divisivel = n1 % n2
+console.log("O primeiro número é divisível pelo segundo?" + (!n1Divisivel))*/
+
+/*const n2Divisivel = n2 % n1
+console.log("O segundo número é divisível pelo primeiro?" + (!n2Divisivel))*/
+
+
+
+
+
