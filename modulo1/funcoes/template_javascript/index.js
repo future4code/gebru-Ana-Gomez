877243318a24824ad92321a1f4802505b9700338
pepinodev <92928197+pepinodev@users.noreply.github.com>
2021-11-19 22:11:44 -0300

    Exercícios funções feito

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,120 @@
+// Exercícios de Interpretação de Código
+// 1 - a) Será impresso 20 e 50, respectivamente.
+// b) A ação seria exercida, porém não apareceria nada no console pelo fato de não imprimir ele.
+// 2 - a) Essa função foi escrita para deixar qualquer string em letras minúsculas e para checar se tem a palavra "cenoura" dentro da frase.
+// b) true / true / false
+
+// Exercícios de Escrita de Código
+// 1 - a)
+/*function imprimirFrase(){
+    console.log("Eu sou a Ana, tenho 22 anos, moro em Ribeirão Preto e sou estudante.")
+ }
+ imprimirFrase()*/
+
+ // b) 
+/* function fraseUsuario (NOME, IDADE, CIDADE, PROFISSAO){
+    const InfoUsuario = `eu sou ${NOME}, tenho ${IDADE} anos, moro em ${CIDADE} e sou ${PROFISSAO}`
+  return InfoUsuario 
+ }
+ console.log(fraseUsuario("Ana", 22, "Ribeirão Preto", "estudante"))*/
+
+ // 2 - a)
+ /*function somarNumeros(n1, n2){
+ return n1 + n2
+ }
+ console.log(somarNumeros(2, 4))*/
+
+// b)
+/*function informarNumero (n1, n2){
+    return n1 >= n2
+}*/
+
+// c)
+/*function identificarNumero(n1){
+    const boolean = n1 % 2 === 0
+    return true
+}*/
+
+// d) 
+/*function imprimirMensagem(frase){
+const frase = "Catatau é o maior"
+console.log(frase.length, frase.toUpperCase())
+imprimirMensagem(frase)
+}*/
+
+// 3 - 
+/*
+function somarNumero(n1, n2){
+return n1 + n2
+}*/
+
+/*function subtrairNumero(n1, n2){
+ return n1 - n2
+}
+
+function multiplicarNumero(n1, n2){
+return n1 * n2
+}
+
+function dividirNumero(n1, n2){
+return n1 / n2
+}*/
+
+
+// Desafios
+// 1 - a)
+/*const impressao = (arrow) =>{
+    console.log(arrow)
+}*/
+// b) 
+/*const doisValores = (arrow1, arrow2) =>{
+    const soma = arrow1 + arrow2
+    doisValores(soma)
+}*/
+
+
+
+// 2 -
+/* const calculeHipotenusa = (angulo1, angulo2) =>{
+    const hipotenusa = (Math.pow(angulo1, 2) + (Match.pow(angulo2)))
+    return Math.sqrt(hipotenusa)
+}
+console.log(calculeHipotenusa(3, 6))
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
