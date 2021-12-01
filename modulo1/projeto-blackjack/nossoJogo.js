f5d2a3199214667e1dd6042ac33491d0f8913fc9
pepinodev <92928197+pepinodev@users.noreply.github.com>
2021-11-30 23:02:48 -0300

    Exercicio sobre Laços feito

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,51 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+console.log("Boas vindas ao jogo de BlackJack!")
+if (confirm("Deseja iniciar uma nova rodada?")){
+     const jogarBlack = ()=>{
+      cartaUsuario1 = comprarCarta()
+      cartaUsuario2 = comprarCarta()
+      cartaPc1 = comprarCarta()
+      cartaPc2 = comprarCarta()
+      pontosUser = cartaUsuario1.valor + cartaUsuario2.valor
+      pontosPc = cartaPc1.valor + cartaPc2.valor
+     }
+
+jogarBlack()
+   
+console.log(`Usuário - cartas ${cartaUsuario1.texto} ${cartaUsuario2.texto}- pontuação: ${pontosUser} `)
+console.log(`Computador - cartas ${cartaPc1.texto} ${cartaPc2.texto} - pontuação: ${pontosPc}`)
+   } else {
+      console.log("Você perdeu! :c")
+   }
+   if (pontosUser === pontosPc){
+      console.log("Deu Empate!")
+   } else if (pontosUser > pontosPc){
+     console.log("O Usuário ganhou!")
+   } else {
+      console.log("O Computador ganhou!")
+   }
+/*const cartaUsuario2 = comprarCarta()
+console.log(carta.texto)
+const cartaPc1 = comprarCarta()
+console.log(carta.texto.valor)
+const cartaPc2 = comprarCarta()
+console.log(carta.texto.valor)
+const somaCartaUsuario = cartaUsuario1 + cartaUsuario2
+const somaCartaPc = cartaPc1 + cartaPc2
+console.log(somaCartaPc)
+//console.log(`Usuário - cartas: ${cartaUsuario1(carta.valor)(carta.texto)} ${cartaUsuario2(carta.valor)(carta.texto)} - pontuação: ${somaCartaUsuario}`)
+//console.log(`Computador - cartas: ${cartaPc1(carta.valor)(carta.texto)} ${cartaPc2(carta.valor)(carta.texto)} - pontuação: ${somaCartaPc}`)
+const carta = comprarCarta()
+console.log(carta.valor)
+console.log(carta.texto)*/
