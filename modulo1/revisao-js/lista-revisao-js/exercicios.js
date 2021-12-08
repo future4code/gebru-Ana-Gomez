12044b4030616ed56ef708a50403cde88ab310eb
pepinodev <92928197+pepinodev@users.noreply.github.com>
2021-12-08 19:34:47 -0300

    Exercício 15 a feito

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -145,7 +145,10 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-
+    consultas.sort(function(a,b) {
+        return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
+    });
+    return consultas
 }
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
